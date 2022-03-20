d6b2b157ecc79cf6a07723c781284d147035f387
Pirshtuk <pirstuk@gmail.com>
2022-03-20 19:34:23 +0300

    show_in_excel()

--- R/utils.R
+++ R/utils.R
@@ -8,3 +8,27 @@
 compact <- function(x) {
   x[!vapply(x, is.null, logical(1))]
 }
+
+
+#' Show data.frame in excel
+#' create temp file
+#' @param .data
+#'
+#' @return  data.frame
+#' @export
+#'
+#' @examples
+#'
+#' mtcars |>
+#'  show_in_excel() |>
+#'  dplyr::select(1:2)#'
+#'
+#'
+show_in_excel <- function(.data) {
+  if (interactive()) { # avoid unvanted Excel execution
+    tmp <- tempfile(fileext = ".csv")
+    readr::write_excel_csv(.data,tmp,delim=";")
+    fs::file_show(tmp)
+    browseURL(tmp)
+  }
+}
